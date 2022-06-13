530914a2e4004684ec0d09b502bf77fbf0518b6c
melvin giebels <melvin@LAPTOP-740SEOHC.localdomain>
2022-06-13 14:49:33 +0200

    Added working Routing module

---
+++ apps/angular-frontend/src/app/pages/following/following.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "the-circle-following",
+  templateUrl: "./following.component.html",
+  styleUrls: ["./following.component.scss"],
+})
+export class FollowingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
