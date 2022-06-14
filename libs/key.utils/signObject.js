42082edc725ff44435c299a63ad889e5286e02e0
Stijn van Houwelingen <stijn@van-houwelingen.net>
2022-06-14 13:43:48 +0000

    changed some names, changed folder name and added keyGen.js

--- libs/key.utils/signObject.js
+++ libs/key.utils/signObject.js
@@ -6,14 +6,14 @@ import crypto from 'crypto'
 /**
  * Creates digital signature with a hash and a UUID.
  * @param {object} object - The object or message that will be signed
- * @param {string} privKeyString - The private key in string form
+ * @param {string} privateKeyString - The private key in string form
  * @returns {string} Returns the encrypted packet with hash and UUID
  */
 
-export default function signObject(object, privKeyString) {
-    let privKey = new NodeRSA
-    privKey = privKey.importKey(privKeyString);
+export default function signObject(object, privateKeyString) {
+    let privateKey = new NodeRSA
+    privateKey = privateKey.importKey(privateKeyString);
     const unsigned = [hash(object), crypto.randomUUID()];
     console.log("signObject: object is signed");
-    return privKey.encryptPrivate(unsigned, 'base64');
+    return privateKey.encryptPrivate(unsigned, 'base64');
 }
--- libs/key.utils/verifyObject.js
+++ libs/key.utils/verifyObject.js
@@ -4,19 +4,19 @@ import NodeRSA from 'node-rsa';
  * Verifies digital signature of an encrypted hashed object and retrieves the UUID
  * @param {object} packet - The encrypted packet which contains the hash and the UUID.
  * @param {object} object - The object or message to be verified
- * @param {string} pubKeyString - The public key in string form
+ * @param {string} publicKeyString - The public key in string form
  * @typedef {Object} returnItem 
  * @property {boolean} isValid - The object hash compared to the packet hash.
  * @property {string} UUID - The UUID that was sent in the packet
  * @returns {returnItem}
  */
 
- export default function verifyObject(packet, object, pubKeyString) {
-    let pubKey = new NodeRSA;
-    pubKey = pubKey.importKey(pubKeyString)
+ export default function verifyObject(packet, object, publicKeyString) {
+    let publicKey = new NodeRSA;
+    publicKey = publicKey.importKey(publicKeyString)
     const objectHash = hash(object);
 
-    let [decryptedHash,UUID] = pubKey.decryptPublic(packet, 'json');
+    let [decryptedHash,UUID] = publicKey.decryptPublic(packet, 'json');
     const isValid = objectHash === decryptedHash;
     console.log("\n")
     console.log("verifyObject:")
