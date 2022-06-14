f462752e7f6d6bbd6b0c8a9b2e599fced0592ce0
maarten <maarten@LAPTOP-Q21ELVRV.localdomain>
2022-06-14 15:05:34 +0200

    added routes and crudcontroller

--- apps/express-backend/src/app/router/room.router.ts
+++ apps/express-backend/src/app/router/room.router.ts
@@ -1,11 +1,11 @@
 import * as router from "express";
 const routes = router.Router();
 import { CrudController } from "../controllers/crud.controller";
-import { chat } from "../../schemas/chat.model";
-const controller: CrudController = new CrudController(chat);
+import { room } from "../../schemas/room.model";
+const controller: CrudController = new CrudController(room);
 
 routes.get("", controller.getAll);
 routes.get("/:id", controller.getById);
-routes.post("/:id", controller.create);
+routes.post("/:id", controller.update);
 
 export default routes;
