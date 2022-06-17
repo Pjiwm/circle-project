1a6510ec526ec9b18b5510bed4b05f0d48b55e30
melvingiebels <melvingiebels@gmail.com>
2022-06-17 15:15:48 +0200

    Working encrypt and decrypt login

--- apps/express-backend/src/app/controllers/security.conrtoller.ts
+++ apps/express-backend/src/app/controllers/security.conrtoller.ts
@@ -14,18 +14,16 @@ export class SecurityController {
   };
   login = async function (req, res, next) {
     try {
-      const person: Person = await PersonModel.findOne({
+      const personPromise: Person = await PersonModel.findOne({
         name: req.body.name,
       }).exec();
-      console.log(person);
-      console.log(
-        "signature: " + req.body.signature + "Publickey: " + person.publicKey+"Name: " + req.body.name);
-      const decryptedMessage = rsaService.decrypt(req.body.signature,person.publicKey,{name: req.body.name});
-      console.log(decryptedMessage);
+
+      // Convert promise to object
+      const person: Person = new Person(personPromise._id, personPromise.name, personPromise.publicKey, personPromise.satochi, personPromise.followed);
+
+      const decryptedMessage = rsaService.decrypt(req.body.signature,person.publicKey,{name: req.body.name});;
       if (decryptedMessage) {
-        console.log(process.env.PRIVATEKEY_SERVER);
         const signature = rsaService.encrypt({person: person},process.env.PRIVATEKEY_SERVER);
-        console.log(signature);
         res.status(200).send({ signature: signature, person: person });
       }
     } catch (err) {
