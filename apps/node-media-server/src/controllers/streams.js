d98b3a529e9c2cb5ebffe20c7c048d3055121422
Pistrie <sylvester.roos@xs4all.nl>
2022-06-20 11:13:00 +0200

    minor fix

--- apps/node-media-server/src/controllers/streams.js
+++ apps/node-media-server/src/controllers/streams.js
@@ -1,6 +1,6 @@
 // TODO implement getting livestream based on username
-const getLivestream = async (req, res) => {
+const getLivestream = (req, res) => {
   res.status(500).json({ msg: "method not implemented" });
 };
 
-module.exports = getLivestream;
+module.exports = { getLivestream };
