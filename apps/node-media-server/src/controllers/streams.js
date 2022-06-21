e9aa691b0835973c49f7843e6759f128e8a4e15f
Pistrie <sylvester.roos@xs4all.nl>
2022-06-21 13:16:12 +0200

    moved getLatestDate to it's own file, renamed function

--- apps/node-media-server/src/controllers/streams.js
+++ apps/node-media-server/src/controllers/streams.js
@@ -6,35 +6,22 @@ const {
 /**
  * Get the manifest of the livestream by username of the transparent person
  */
-const getLivestreamManifestByUsername = (req, res) => {
+const getLivestreamManifestSignatureByUsername = (req, res) => {
   logger.log("getLivestreamManifestByUsername called...");
   res.status(500).json({ msg: "method not implemented" });
 
   const username = req.params.username;
 
-  // read directory, load timestamps into array
-  const timestampsFolder = `${process.env.FFMPEG_ROOT_OUTPUT_FOLDER}/${username}-streams`;
-  logger.log(`TIMESTAMPS FOLDER ==> ${timestampsFolder}`);
-  const timestamps = fs
-    .readdirSync(timestampsFolder)
-    .map((date) => new Date(date));
-
-  // disgusting imperative way to check latest date
-  if (timestamps.length === 0)
-    res.status(404).json({ msg: "No stream available for this user" });
-  let latestDate = timestamps[0];
-  for (let i = 1; i < timestamps.length; i++) {
-    let currentDate = timestamps[i];
-    if (currentDate < latestDate) {
-      latestDate = currentDate;
+  // TODO call utils/get-latest-date
+  getLatestDateFromDirectory(username, (error, result) => {
+    if (error) {
+      res.status(404).json({ msg: "No stream found" });
     }
-  }
-
-  // TODO send manifest
-
-  // express.static(
-  //   `${process.env.FFMPEG_ROOT_OUTPUT_FOLDER}/${username}-streams/NEWEST TIMESTAMP HERE/${username}.m3u8`
-  // );
+    if (result) {
+      const latestDate = result.latestDate;
+      // TODO send manifest
+    }
+  });
 };
 
-module.exports = { getLivestreamManifestByUsername };
+module.exports = { getLivestreamManifestSignatureByUsername };
--- apps/node-media-server/src/routes/streams.js
+++ apps/node-media-server/src/routes/streams.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getLivestreamManifestByUsername } = require("../controllers/streams");
+const {
+  getLivestreamManifestSignatureByUsername,
+} = require("../controllers/streams");
 
-router.route("/:username").get(getLivestreamManifestByUsername);
+router.route("/:username").get(getLivestreamManifestSignatureByUsername);
 
 module.exports = router;
