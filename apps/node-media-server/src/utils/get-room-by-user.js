7111a85dbd8eb77650ab32b9f4e8c41405d38efa
Pistrie <sylvester.roos@xs4all.nl>
2022-06-21 17:13:15 +0200

    added code to get userId from mongo based on username

---
+++ apps/node-media-server/src/utils/get-room-by-user.js
@@ -0,0 +1,28 @@
+const roomSchema = require("../schemas/room");
+const personSchema = require("../schemas/person");
+
+/**
+ * Private method to get userId from mongo based on username
+ * @param username
+ */
+const privateGetUserIdFromUsername = async (username) => {
+  const personPromise = await personSchema
+    .findOne({
+      name: username,
+    })
+    .exec();
+
+  return personPromise._id;
+};
+
+const getRoomIdFromUsername = async (username) => {
+  const roomPromise = await roomSchema
+    .findOne({
+      streamer: privateGetUserIdFromUsername(username), // this is going to error out because it's a promise
+    })
+    .exec();
+
+  return roomPromise._id;
+};
+
+module.exports = getRoomIdFromUsername;
