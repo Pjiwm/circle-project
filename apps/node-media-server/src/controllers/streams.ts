0190799483c4f9dd1e92e2d0b4229b197006fca1
Pistrie <sylvester.roos@xs4all.nl>
2022-06-22 14:28:41 +0200

    now able to request signature of a single segment

--- apps/node-media-server/src/controllers/streams.ts
+++ apps/node-media-server/src/controllers/streams.ts
@@ -1,10 +1,12 @@
 import * as fs from "fs";
 import { RsaService } from "../../../../libs/keyUtils";
-import { PUBLIC_SERVER_KEY, PRIVATE_SERVER_KEY } from "../../../../libs/key";
+import { PRIVATE_SERVER_KEY } from "../../../../libs/key";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const logger = require("tracer").console();
 const {
   getLatestDateFromOutputDirectory,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require("../../../../libs/get-latest-date");
 
 /**
@@ -65,4 +67,101 @@ const getLivestreamManifestSignatureByUsername = (req, res) => {
   });
 };
 
-module.exports = { getLivestreamManifestSignatureByUsername };
+const getStreamSegmentsSignaturesByUsername = (req, res) => {
+  logger.log("getStreamSegmentsSignaturesByUsername called...");
+
+  const username = req.params.username;
+  const segment = parseInt(req.params.segment);
+  // const segments = req.params.segments.split("-");
+  // const startSegment = parseInt(segments[0]);
+  // const endSegment = parseInt(segments[1]);
+  // console.log(startSegment);
+  // console.log(endSegment);
+
+  // return signature of one segment
+  getLatestDateFromOutputDirectory(username, (error, result) => {
+    if (result) {
+      console.log("folder exists");
+      const latestDate = result.latestDate;
+
+      const segmentFile = `${process.env.FFMPEG_ROOT_OUTPUT_FOLDER}/${username}-streams/${latestDate}/${username}${segment}.ts`;
+      console.log(segmentFile);
+
+      // check if segment exists
+      fs.stat(segmentFile, (error, result) => {
+        if (result) {
+          console.log("segment exists");
+          const rsaService: RsaService = new RsaService();
+
+          const segmentFileString = fs.readFileSync(segmentFile).toString();
+          const segmentFileStringSignature = rsaService.encrypt(
+            segmentFileString,
+            PRIVATE_SERVER_KEY
+          );
+
+          res.status(200).json({ signature: segmentFileStringSignature });
+        } else {
+          res.status(500).json({ msg: "segment does not exist" });
+        }
+      });
+    } else {
+      res.status(500).json({ msg: "folder does not exist" });
+    }
+  });
+
+  // if (startSegment === endSegment) {
+  //   // return signature of one segment
+  //   getLatestDateFromOutputDirectory(username, (error, result) => {
+  //     if (result) {
+  //       console.log("folder exists");
+  //       const latestDate = result.latestDate;
+  //
+  //       const segmentFile = `${process.env.FFMPEG_ROOT_OUTPUT_FOLDER}/${username}-streams/${latestDate}/${username}${startSegment}.ts`;
+  //       console.log(segmentFile);
+  //
+  //       // check if segment exists
+  //       fs.stat(segmentFile, (error, result) => {
+  //         if (result) {
+  //           console.log("segment exists");
+  //           const rsaService: RsaService = new RsaService();
+  //
+  //           const segmentFileString = fs.readFileSync(segmentFile).toString();
+  //           const segmentFileStringSignature = rsaService.encrypt(
+  //             segmentFileString,
+  //             PRIVATE_SERVER_KEY
+  //           );
+  //
+  //           res.status(200).json({ signature: segmentFileStringSignature });
+  //         } else {
+  //           res.status(500).json({ msg: "segment does not exist" });
+  //         }
+  //       });
+  //     } else {
+  //       res.status(500).json({ msg: "folder does not exist" });
+  //     }
+  //   });
+  // }
+  // else {
+  //   getLatestDateFromOutputDirectory(username, (error, result) => {
+  //     if (result) {
+  //       console.log("folder exists");
+  //       const latestDate = result.latestDate;
+  //
+  //       const segmentFile = `${process.env.FFMPEG_ROOT_OUTPUT_FOLDER}/${username}-streams/${latestDate}/${username}${startSegment}.ts`;
+  //
+  //       // create array of numbers (inclusive) starting with startSegment and ending with endSegment
+  //       const range = [...Array(endSegment - startSegment + 1).keys()].map(
+  //         (x) => x + startSegment
+  //       );
+  //
+  //       const signatures;
+  //       range.map((s) => fs.stat());
+  //     }
+  //   });
+  // }
+};
+
+module.exports = {
+  getLivestreamManifestSignatureByUsername,
+  getStreamSegmentsSignaturesByUsername,
+};
